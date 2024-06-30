b00bdfed52f08b2f8cac39546bc444fb7b74a21c
Alishkoo <baisholanov.alibek@gmail.com>
2024-06-30 12:47:00 +0500

    added prisma/postgres/trpc

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,5 +1,7 @@
 import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
+import { trpc } from "../_trpc/client";
+import { useEffect } from "react";
 
 const Page = () => {
     const router = useRouter();
@@ -7,6 +9,17 @@ const Page = () => {
     const searchParams = useSearchParams()
     const origin = searchParams.get("origin");
 
+
+    const{data, isLoading, error} = trpc.authCallback.useQuery()
+
+    useEffect(() => {
+        if (data && data.success) {
+            router.push(origin ? `/${origin}` : `/dashboard`);
+        }
+        else if(error?.data?.code === "UNAUTHORIZED") {
+            router.push(`/sign-in`)
+        }
+    }, [data, origin, router]);
     
 }
 
